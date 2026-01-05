a13a41a5f1c0446cce7542c6380666398fa968b5
Ramin Atayee <raminatayeeofficial@gmail.com>
2026-01-05 23:24:00 +0430

    if else and if else if example

---
+++ main.js
@@ -0,0 +1,31 @@
+// if with else ________________
+var age = 10;
+if(age >18){
+    console.log("True");
+}
+else{
+    console.log("False");
+}
+
+// if else if Example1
+let salary = 20000;
+if(salary> 20000){
+    console.log("He work 10 hour");
+}
+else if(salary=18000){
+    console.log("He work 8 hour")
+}
+else if(salary<15000){
+    console.log("He work 5 hour")
+}
+// if else if Example2
+let child = 6;
+if(child > 7){
+    console.log("He con go to school");
+}
+else if(child >= 7){
+    console.log("He con go to school")
+}
+else if(child < 7){
+    console.log("He should go to prechool")
+}
